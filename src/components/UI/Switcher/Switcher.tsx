5e69f8e4d68960dd7695daea0b9e93aea704e439
First Last <borisov.sasha1999@yandex.by>
2025-01-04 22:47:26 +0300

    Adding icons to Switcher Component for Dark/Light mode of App

--- src/components/UI/Switcher/Switcher.tsx
+++ src/components/UI/Switcher/Switcher.tsx
@@ -1,10 +1,16 @@
 import './Switcher.scss';
+import day from '../../../assets/images/day_icon.svg';
+import night from '../../../assets/images/night_icon.svg';
 
 export const Switcher = () => {
-    return (
-        <label className="switcher">
+   return (
+      <div className="flex  items-center gap-1">
+         <img className="h-7 w-7" src={day} alt="Sun icon" />
+         <label className="switcher">
             <input type="checkbox" />
             <span className="slider"></span>
-        </label>
-    );
+         </label>
+         <img className="h-7 w-7" src={night} alt="Night icon" />
+      </div>
+   );
 };
