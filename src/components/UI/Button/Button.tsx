b10dbc2f28b29e1e2b18cc159b2755bb916660f3
First Last <borisov.sasha1999@yandex.by>
2025-01-07 19:57:07 +0300

    Modify the size Button Component

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 export const Button = ({ children, disabled, onClick }: ButtonProps) => {
    return (
       <button
-         className="w-32 h-8 py-1 px-2 bg-transparent text-blue-400 border-blue-400 border-2 rounded-md transition-colors duration-300 ease-in-out
+         className="w-24 h-8 py-1 px-2 bg-transparent text-blue-400 border-blue-400 border-2 rounded-md transition-colors duration-300 ease-in-out
             cursor-pointer hover:bg-blue-400 hover:text-white active:shadow-md"
          disabled={disabled}
          onClick={onClick}
