3cd1f1b749057296052856eedd5f3d1d3c057dc7
First Last <borisov.sasha1999@yandex.by>
2025-01-07 20:27:18 +0300

    Current date for Menu Tab

--- src/components/UI/Menu/Menu.tsx
+++ src/components/UI/Menu/Menu.tsx
@@ -2,13 +2,14 @@ import { Switcher } from '../Switcher/Switcher';
 import { Navbar } from '../Navbar/Navbar';
 
 export const Menu = () => {
+   const currentYear = new Date().getFullYear();
    return (
       <div className="h-full w-30vw py-5 flex flex-col justify-between items-center border-r-2">
          <Switcher />
          <Navbar />
          <div className="text-center">
             <p className="font-bold"> All rights reserved </p>
-            <p> ©2025 </p>
+            <p> ©{currentYear} </p>
          </div>
       </div>
    );
