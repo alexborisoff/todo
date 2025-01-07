6d7ae57cbee32738c8317fcd8709f51b2a0d878f
First Last <borisov.sasha1999@yandex.by>
2025-01-07 20:09:05 +0300

    Switching between pages Todos and About

--- src/components/UI/Navbar/Navbar.tsx
+++ src/components/UI/Navbar/Navbar.tsx
@@ -2,9 +2,13 @@ import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
    return (
-      <div className="px-10 flex flex-col justify-center border-2 h-1/3">
-         <Link to="/"> Todos </Link>
-         <Link to="/about_us"> About </Link>
+      <div className="px-10 h-1/5 flex flex-col justify-center border-2 rounded-md text-xl">
+         <Link className="hover:text-blue-400 transition-colors duration-300 ease-in-out" to="/">
+            TODOS
+         </Link>
+         <Link className="hover:text-blue-400 transition-colors duration-300 " to="/about_us">
+            ABOUT
+         </Link>
       </div>
    );
 };
