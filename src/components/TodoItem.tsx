a2f9bf5ba28dd722bfe011df625b28f42eab5d1e
First Last <borisov.sasha1999@yandex.by>
2025-01-07 20:07:18 +0300

    Functional for change status of Task to completed

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import { Todo } from '../data/local/models/Todo';
 import { Button } from './UI/Button/Button';
+
 
 interface TodoItemProps {
    todo: Todo;
@@ -7,16 +9,23 @@ interface TodoItemProps {
 }
 
 export const TodoItem = ({ todo, remove }: TodoItemProps) => {
+   const [isDone, setIsDone] = useState<boolean>(false);
+
    return (
       <div className="py-1 px-2 flex flex-col justify-between border-solid border-blue-400 rounded border-2 ">
-         <div className="flex flex-row justify-between items-center">
-            <p className="font-bilbo text-xl">
+         <div className="flex flex-row justify-between items-center ">
+            <p className={`font-bilbo text-xl ${isDone ? 'line-through text-gray-300' : ''}`}>
                {todo.id} {todo.title}
             </p>
-            <p>
-               <input type="checkbox" />
+            <div className="flex gap-2 items-center">
+               <input
+                  type="checkbox"
+                  onChange={() => setIsDone(!isDone)}
+                  className="h-5 w-5 border-blue-400 border-none rounded-50per bg-white-600 cursor-pointer transition-colors duration-300 hover:bg-gray-200"
+               />
+               <Button onClick={() => alert('test')}> Edit </Button>
                <Button onClick={() => remove(todo)}> Delete </Button>
-            </p>
+            </div>
          </div>
          <p className="font-ubuntu text-end text-gray-500/90 text-sm">
             created: {todo.createdDate}
