a21cdfe9fd65e9465f555510a84d021c1413ecaa
First Last <borisov.sasha1999@yandex.by>
2025-01-07 20:01:00 +0300

    Creation pages by react-router

---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+import { Menu } from '../components/UI/Menu/Menu';
+
+export const About = () => {
+   return (
+      <section className="h-70vh w-85vw flex justify-center items-center flex-row border-blue-300 border-2 shadow-xl shadow-blue-300">
+         <Menu />
+         <div className="h-full w-55vw flex justify-start flex-col items-center">
+            <h2 className="text-3xl text-blue-500 font-bold"> About Us </h2>
+         </div>
+      </section>
+   );
+};
--- src/pages/Todos.tsx
+++ src/pages/Todos.tsx
@@ -3,8 +3,7 @@ import { Todo } from '../data/local/models/Todo';
 import { todoData } from '../data/remote/models/TodoResponseModel';
 import { TodoForm } from '../components/TodoForm';
 import { TodoList } from '../components/TodoList';
-import { Switcher } from '../components/UI/Switcher/Switcher';
-import { Navbar } from '../components/UI/Navbar/Navbar';
+import { Menu } from '../components/UI/Menu/Menu';
 
 export const Todos = () => {
    const [todos, setTodos] = useState<Todo[]>(todoData);
@@ -23,14 +22,7 @@ export const Todos = () => {
 
    return (
       <section className="h-70vh w-85vw flex justify-center items-center flex-row border-blue-300 border-2 shadow-xl shadow-blue-300">
-         <div className="h-full w-30vw py-5 flex flex-col justify-between items-center">
-            <Switcher />
-            <Navbar />
-            <div className="text-center">
-               <p className="font-bold"> All rights reserved </p>
-               <p> ©2025 </p>
-            </div>
-         </div>
+         <Menu />
          <div className="h-full w-55vw flex justify-start flex-col items-center">
             <h2 className="text-3xl text-blue-500 font-bold"> TODOS </h2>
             <TodoForm create={createTodo} />
