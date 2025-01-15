df874935e2af259e3ac79abf81cbcc37f2252956
Alex Borisov <borisovalexdev@gmail.com>
2025-01-16 00:05:42 +0300

    Main section with border-radius

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,12 +4,12 @@ interface AboutProps {
    changeTheme: () => void;
 }
 
-export const About = ({changeTheme}:AboutProps) => {
+export const About = ({ changeTheme }: AboutProps) => {
    return (
-      <section className="h-70vh w-85vw flex justify-center items-center flex-row border-blue-300 border-2 shadow-xl shadow-blue-300">
+      <section className="rounded-2xl h-70vh w-85vw flex justify-center items-center flex-row border-blue-300 border-2 shadow-xl shadow-blue-300">
          <Menu changeThemeMode={changeTheme} />
          <div className="h-full w-55vw flex justify-start flex-col items-center">
-            <h2 className="text-3xl text-blue-500 font-bold"> About Us </h2>
+            <h2 className="text-3xl text-blue-500 font-bold"> About </h2>
          </div>
       </section>
    );
--- src/pages/Todos.tsx
+++ src/pages/Todos.tsx
@@ -8,7 +8,7 @@ interface TodosProps {
    changeTheme: () => void;
 }
 
-export const Todos = ({changeTheme}:TodosProps) => {
+export const Todos = ({ changeTheme }: TodosProps) => {
    const [todos, setTodos] = useState<Todo[]>([]);
 
    useEffect(() => {
@@ -29,7 +29,7 @@ export const Todos = ({changeTheme}:TodosProps) => {
    };
 
    return (
-      <section className=" h-70vh w-85vw flex justify-center items-center flex-row border-blue-300 border-2 shadow-xl shadow-blue-300 ">
+      <section className="rounded-2xl h-70vh w-85vw flex justify-center items-center flex-row border-blue-300 border-2 shadow-xl shadow-blue-300 ">
          <Menu changeThemeMode={changeTheme} />
          <div className="h-full w-55vw flex justify-start flex-col items-center">
             <h2 className="dark:text-white text-3xl text-blue-500 font-bold"> TODOS </h2>
