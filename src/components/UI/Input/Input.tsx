765b426e9c6a5a89f040aee613f9b4afd62dacad
Alex Borisov <borisovalexdev@gmail.com>
2025-01-16 00:06:27 +0300

    Responsive design for Input Component

--- src/components/UI/Input/Input.tsx
+++ src/components/UI/Input/Input.tsx
@@ -10,7 +10,7 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 export const Input = ({ type, placeholder, value, onChange }: InputProps) => {
    return (
       <input
-         className="dark:bg-slate-400 dark:placeholder:text-white dark:focus:text-white w-96 outline-none p-2 m-1 h-8 rounded-lg border-blue-300 border-2 text-blue-500 transition-colors duration-700 font-bilbo focus:border-blue-700"
+         className="tablet:w-72 desktop:w-96 dark:bg-slate-400 dark:placeholder:text-white dark:focus:text-white  outline-none p-2 m-1 h-8 rounded-lg border-blue-300 border-2 text-blue-500 transition-colors duration-700 font-bilbo focus:border-blue-700"
          type={type}
          onChange={onChange}
          value={value}
