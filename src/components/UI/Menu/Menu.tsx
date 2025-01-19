7c8951ddbd6b098a6baba8b23790a7092683f4b5
Alex Borisov <borisovalexdev@gmail.com>
2025-01-19 17:44:06 +0300

    Adding buttons for switching languages fron En to Ru

--- src/components/UI/Menu/Menu.tsx
+++ src/components/UI/Menu/Menu.tsx
@@ -1,18 +1,37 @@
 import { Switcher } from '../Switcher/Switcher';
 import { Navbar } from '../Navbar/Navbar';
 import { currentYear } from '../../../utils/dates';
+import { useTranslation } from 'react-i18next';
+
+const languages = {
+   en: { nativeName: 'English' },
+   ru: { nativeName: 'Russian' },
+};
 
 interface MenuProps {
    changeThemeMode: () => void;
 }
 
 export const Menu = ({ changeThemeMode }: MenuProps) => {
+   const { t, i18n } = useTranslation();
    return (
       <div className="h-full w-30vw py-5 flex flex-col justify-between items-center border-r-2">
+         <div>
+            {Object.keys(languages).map(language => (
+               <button
+                  type="submit"
+                  key={language}
+                  onClick={() => i18n.changeLanguage(language)}
+                  disabled={i18n.resolvedLanguage === language}
+               >
+                  {language}
+               </button>
+            ))}
+         </div>
          <Switcher changeThemeMode={() => changeThemeMode()} />
          <Navbar />
          <div className="text-center">
-            <p className="font-bold"> All rights reserved </p>
+            <p className="font-bold"> {t('rights')} </p>
             <p> ©{currentYear} </p>
          </div>
       </div>
